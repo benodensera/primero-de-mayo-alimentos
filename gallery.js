b732ecd4e12a4b2382de9200c3ba365408fcddf1
DevTech-Agency <devtechagency.ec@gmail.com>
2026-05-11 14:18:26 -0500

    gallery titles

--- gallery.js
+++ gallery.js
@@ -1,6 +1,10 @@
-const images = document.querySelectorAll(".galleryGrid img");
+const images = document.querySelectorAll(".gallery-item img");
+const titles = document.querySelectorAll(".gallery-item h4");
+
 const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightboxImg");
+const lightboxTitle = document.getElementById("lightboxTitle");
+
 const closeBtn = document.getElementById("closeLightbox");
 const nextBtn = document.getElementById("nextBtn");
 const prevBtn = document.getElementById("prevBtn");
@@ -12,13 +16,18 @@ images.forEach((img, index) => {
     img.addEventListener("click", () => {
         currentIndex = index;
         showImage();
+
         lightbox.classList.add("active");
+        document.body.classList.add("no-scroll");
     });
 });
 
+
 function showImage() {
     lightboxImg.src = images[currentIndex].src;
+    lightboxTitle.textContent = titles[currentIndex].textContent;
 }
+
 
 nextBtn.addEventListener("click", () => {
     currentIndex = (currentIndex + 1) % images.length;
@@ -30,40 +39,15 @@ prevBtn.addEventListener("click", () => {
     showImage();
 });
 
-closeBtn.addEventListener("click", () => {
+function closeLightbox() {
     lightbox.classList.remove("active");
-});
+    document.body.classList.remove("no-scroll");
+}
+
+closeBtn.addEventListener("click", closeLightbox);
 
 lightbox.addEventListener("click", (e) => {
     if (e.target === lightbox) {
-        lightbox.classList.remove("active");
-    }
-});
-
-
-
-images.forEach((img, index) => {
-    img.addEventListener("click", () => {
-        currentIndex = index;
-        showImage();
-        lightbox.classList.add("active");
-
-        document.body.classList.add("no-scroll"); 
-    });
-});
-
-
-
-closeBtn.addEventListener("click", () => {
-    lightbox.classList.remove("active");
-    document.body.classList.remove("no-scroll"); 
-});
-
-
-
-lightbox.addEventListener("click", (e) => {
-    if (e.target === lightbox) {
-        lightbox.classList.remove("active");
-        document.body.classList.remove("no-scroll");
+        closeLightbox();
     }
 });
